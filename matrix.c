829a65db6313960552f7e65d8bc82fd56f5df8b2
Esau Hutcherson <esau.hutcherson@bison.howard.edu>
2022-12-13 18:41:27 +0000

    finalized changes

---
+++ matrix.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+// Esau Hutcherson
+//outside resource/help: https://medium.com/tech-vision/parallel-matrix-multiplication-c-parallel-processing-5e3aadb36f27
+int MAX;
+
+int main() {
+  srand(time(0));  
+
+  char input[3];
+  printf("Enter the size of matrix you want to initialize: ");
+  fgets(input, 3, stdin);
+  MAX = atoi(input);
+  
+  int matA[MAX][MAX]; 
+  int matB[MAX][MAX]; 
+
+  int matSumResult[MAX][MAX];
+  int matDiffResult[MAX][MAX]; 
+  int matProductResult[MAX][MAX]; 
+
+  struct args_t {
+    int (*matA)[MAX];
+    int (*matB)[MAX];
+    int (*res)[MAX];
+    int row;
+  };
+
+  void fillMatrix(int matrix[MAX][MAX]) {
+      for(int i = 0; i<MAX; i++) {
+          for(int j = 0; j<MAX; j++) {
+              matrix[i][j] = rand()%10+1;
+          }
+      }
+  }
+
+  void printMatrix(int matrix[MAX][MAX]) {
+      for(int i = 0; i<MAX; i++) {
+          for(int j = 0; j<MAX; j++) {
+              printf("%5d", matrix[i][j]);
+          }
+          printf("\n");
+      }
+      printf("\n");
+  }
+
+  
+  void* computeSum(void* args) { 
+    struct args_t* arg = (struct args_t*) args;
+    int offset = arg->row * MAX;
+    for (int i=0; i<MAX; i++) {
+      int added = arg->matA[0][i+offset] + arg->matB[0][i+offset];
+      arg->res[0][i+offset] = added;
+    }
+  }
+
+
+  void* computeDiff(void* args) { 
+    struct args_t* arg = (struct args_t*) args;
+    int offset = arg->row * MAX;
+    for (int i=0; i<MAX; i++) {
+      int added = arg->matA[0][i+offset] - arg->matB[0][i+offset];
+      arg->res[0][i+offset] = added;
+    }
+  }
+
+  void* computeProduct(void* args) { 
+    struct args_t* arg = (struct args_t*) args;
+    int offset = arg->row * MAX;
+    for (int i=0; i<MAX; i++) {
+      int added = arg->matA[0][i+offset] * arg->matB[0][i+offset];
+      arg->res[0][i+offset] = added;
+    }
+  }
+
+  fillMatrix(matA);
+  fillMatrix(matB);
+
+  printf("Matrix A:\n");
+  printMatrix(matA);
+  printf("Matrix B:\n");
+  printMatrix(matB);
+
+  pthread_t threads[MAX];
+  struct args_t args[MAX];
+  
+  // Sum
+  for (int i=0; i<MAX; i++) {
+    args[i].matA = matA;
+    args[i].matB = matB;
+    args[i].res = matSumResult;
+    args[i].row = i;
+    int error = pthread_create(&threads[i], NULL, &computeSum, (void *) &args[i]);
+    if (error) {
+      printf("Thread creation failed!");
+      exit(1);
+    }
+  }
+  
+  // waiting for the threads
+  for (int i=0; i<MAX; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  
+  // Difference
+  for (int i=0; i<MAX; i++) {
+    args[i].matA = matA;
+    args[i].matB = matB;
+    args[i].res = matDiffResult;
+    args[i].row = i;
+    int error = pthread_create(&threads[i], NULL, &computeDiff, (void *) &args[i]);
+    if (error) {
+      printf("Thread creation failedd!");
+      exit(1);
+    }
+  }
+  
+  // waiting for the threads
+  for (int i=0; i<MAX; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  
+  // Product
+  for (int i=0; i<MAX; i++) {
+    args[i].matA = matA;
+    args[i].matB = matB;
+    args[i].res = matProductResult;
+    args[i].row = i;
+    int error = pthread_create(&threads[i], NULL, &computeProduct, (void *) &args[i]);
+    if (error) {
+      printf("Thread creation failed!");
+      exit(1);
+    }
+  }
+  
+  for (int i=0; i<MAX; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  printf("A + B:\n");
+  printMatrix(matSumResult);
+  printf("A - B:\n");
+  printMatrix(matDiffResult);
+  printf("A * B:\n");
+  printMatrix(matProductResult);
+  return 0;
+}
